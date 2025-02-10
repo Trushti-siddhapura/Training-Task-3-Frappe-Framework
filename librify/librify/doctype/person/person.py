7132e908aa04b1473989e39a373463a2dfb14393
Sanskar <trushti@sanskar.com>
2025-02-10 18:53:39 +0530

    AddedChangableFile

---
+++ librify/librify/doctype/person/person.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025, admin and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class Person(Document):
+ 	def get_full_name(self):
+        	return f"{self.fname} {self.sname}"
+	# def validate(self):
+	# 	if self.age <= 18:
+	# 		frappe.throw("This age is underage you can't vote!")
+	# 		# frappe.msgprint("This age is underage, you can't vote!") 
+
+
+# doc=frappe.get_doc("Person","This is Person 1")
+# print(doc.get_full_name())
+# doc=frappe.get_doc("Person","This is Person 2")
+# print(doc.get_full_name())		
